1759abd16e3c57077f8250d44554c3b248d746f5
ElSergio217 <sergioa.garcia217@gmail.com>
2017-01-20 20:06:29 -0500

    files

---
+++ Assets/Cam.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+ function Start (){
+ 
+     if(GetComponent.<NetworkView>().isMine){
+         GetComponent(Camera).enabled = true;
+     }
+     else{
+         GetComponent(Camera).enabled = false;
+     }
+ }
+function Update () {
+
+}
