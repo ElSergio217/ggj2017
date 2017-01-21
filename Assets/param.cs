1639a506f47d52625751f2da8a2de3b624b93692
ElSergio217 <sergioa.garcia217@gmail.com>
2017-01-21 01:26:34 -0500

    files

--- Assets/param.cs
+++ Assets/param.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class param : MonoBehaviour {
     public int _band;
     public float _startScale, _scaleMultiplier;
-
+    public bool useBuffer;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.localScale = new Vector3(transform.localScale.x,(AudioPeer._freqBand[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useBuffer == true)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer._bandBuffer[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer._freqBand[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+        }
+    }
 }
